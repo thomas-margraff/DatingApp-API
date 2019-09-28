d53f0c0833c68d10a2bb09f8edf3839190b8daaa
thomas-margraff <tmargraff@gmail.com>
2019-09-28 11:49:35 -0700

    Add project files.

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _context { get; }
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/values
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return Ok(users);
+
+        }
+
+
+    }
+}
---
+++ DatingApp.API/Migrations/20190905004645_FixedUserEntitySpelling.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class FixedUserEntitySpelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PassworSalt",
+                table: "Users",
+                newName: "PasswordSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "PassworHash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "PassworSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PassworHash");
+        }
+    }
+}
